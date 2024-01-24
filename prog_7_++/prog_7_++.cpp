8e321104dd928711fcaff8af0f840b4b8f84a03b
chilimpampa <konovalovdan22072004@mail.ru>
2024-01-24 15:55:35 +0700

    вывод данных от пользователя

--- prog_7_++/prog_7_++.cpp
+++ prog_7_++/prog_7_++.cpp
@@ -118,3 +118,101 @@ namespace apartment {
 	void ApartmentDataBase::AddHouse(const House& house) {
 		houses.push_back(house);
 	}
+
+	void ApartmentDataBase::PrintOwnerList() const {
+		if (owners.empty()) {
+			std::cout << "          Данных о владельце нет..." << std::endl;
+		}
+		else {
+			std::cout << "          Данные о владельце" << std::endl;
+			std::cout << "-----------------------------------------" << std::endl;
+			for (const Owner& owner : owners) {
+				std::cout << "Фамилия: " << owner.GetFIO_OWNER() << std::endl;
+				std::cout << "Имя: " << owner.GetNAME_OWNER() << std::endl;
+				std::cout << "-----------------------------------------" << std::endl;
+			}
+		}
+	}
+
+	// Дополнение к методу ApartmentDataBase::PrintFlatList
+	void ApartmentDataBase::PrintFlatList() const {
+		if (flats.empty()) {
+			std::cout << "          Данных о квартире нет..." << std::endl;
+		}
+		else {
+			std::cout << "Общее количество квартир: " << GetTotalApartments() << std::endl;
+			std::cout << "          Данные о квартире" << std::endl;
+			std::cout << "-----------------------------------------" << std::endl;
+			for (const Flat& flat : flats) {
+				std::cout << "Адрес квартиры: " << flat.GetSTREET() << std::endl;
+				std::cout << "Номер квартиры: " << flat.GetNUM_FLAT() << std::endl;
+				std::cout << "Кол-во комнат: " << flat.GetKOL_ROOMS() << std::endl;
+				std::cout << "Площадь квартиры (кв. м): " << flat.GetSQUARE() << std::endl;
+				std::cout << "-----------------------------------------" << std::endl;
+			}
+		}
+	}
+
+	// Дополнение к методу ApartmentDataBase::PrintFlatList
+	void ApartmentDataBase::PrintSortedFlatList() const {
+		vector<Flat> sortedFlats = flats;  // Создание копии вектора квартир
+		sort(sortedFlats.begin(), sortedFlats.end(), [](const Flat& a, const Flat& b) {
+			return a.GetSQUARE() < b.GetSQUARE();  // Сортировка по площади квартиры
+			});
+
+		// Вывод отсортированного списка квартир
+		for (const Flat& flat : sortedFlats) {
+			cout << "Адрес квартиры: " << flat.GetSTREET() << endl;
+			cout << "Номер квартиры: " << flat.GetNUM_FLAT() << endl;
+			cout << "Кол-во комнат: " << flat.GetKOL_ROOMS() << endl;
+			cout << "Площадь квартиры (кв. м): " << flat.GetSQUARE() << endl;
+			cout << "-----------------------------------------" << endl;
+		}
+	}
+
+	void ApartmentDataBase::PrintConciergeList() const {
+		if (concierges.empty()) {
+			std::cout << "          Данных о консьерже нет..." << std::endl;
+		}
+		else {
+			std::cout << "          Данные о консьерже" << std::endl;
+			std::cout << "-----------------------------------------" << std::endl;
+			for (const Concierge& concierge : concierges) {
+				std::cout << "Фамилия консьержа: " << concierge.GetFIO_CONCIERGE() << std::endl;
+				std::cout << "Имя консьержа: " << concierge.GetNAME_CONCIERGE() << std::endl;
+				std::cout << "-----------------------------------------" << std::endl;
+			}
+		}
+	}
+
+	void ApartmentDataBase::PrintDoorwayList() const {
+		if (doorways.empty()) {
+			std::cout << "          Данных о подъезде нет..." << std::endl;
+		}
+		else {
+			std::cout << "          Данные о подъезде" << std::endl;
+			std::cout << "-----------------------------------------" << std::endl;
+			for (const Doorway& doorway : doorways) {
+				std::cout << "Номер подъезда: " << doorway.GetNUM_DOORWAY() << std::endl;
+				std::cout << "Кол-во квартир: " << doorway.GetKOl_FLAT() << std::endl;
+				std::cout << "-----------------------------------------" << std::endl;
+			}
+		}
+	}
+
+	void ApartmentDataBase::PrintHouseList() const {
+		if (houses.empty()) {
+			std::cout << "          Данных о доме нет..." << std::endl;
+		}
+		else {
+			std::cout << "-----------------------------------------" << std::endl;
+			std::cout << "          Данные о доме" << std::endl;
+			std::cout << "-----------------------------------------" << std::endl;
+			for (const House& house : houses) {
+				std::cout << "Название улицы: " << house.GetSTREET() << std::endl;
+				std::cout << "Номер дома: " << house.GetNUM_HOUSE() << std::endl;
+				std::cout << "Кол-во жильцов: " << house.GetKOL_REZIDENTS() << std::endl;
+				std::cout << "-----------------------------------------" << std::endl;
+			}
+		}
+	}
